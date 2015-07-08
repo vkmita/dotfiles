1c558277d032e3323143d209ec825710b9bf6f87
vkmita <me@vkmita.com>
2015-07-08 15:41:04 -0700

    add custom ruby gem ENV vars

--- bash/env.sh
+++ bash/env.sh
@@ -11,6 +11,11 @@ export RBXOPT="-Xrbc.db" # do not clutter up everything with .rbc files when usi
 export HISTSIZE=1000000 #size of history
 export HISTCONTROL=ignorespace:ignoredups # ignore dulicates, cmds beginning with space
 
+export RUBY_VERSION=2.2.2
+export INSTALLATION_DIRECTORY=/Users/vkmita/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems
+export RUBY_EXECUTABLE=/Users/vkmita/.rbenv/versions/2.2.2/lib/ruby/bin/ruby
+export GEM_PATH=/Users/vkmita/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems
+
 #in case-sensitive autocompletion
 tty -s && bind 'set completion-ignore-case on'
 
