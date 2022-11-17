5ee7fda0cb8090f2b05e6c4727754371d6f83742
Victor Kmita <me@vkmita.com>
2022-11-17 17:10:44 +0100

    add new aliases

--- bash/alias.sh
+++ bash/alias.sh
@@ -8,9 +8,14 @@ alias l="ls -lah" #ls all+long+humanized filesizes
 
 f(){ find . -iname "*$1*"; }
 
-function git-find-merge() {
-  git rev-list $1..master --ancestry-path | grep -f <(git rev-list $1..master --first-parent) | tail -1
-}
-
 # GIT
 alias master='git co master'
+alias staging='git co staging'
+alias prod='git co production'
+
+alias main='git checkout main && git pull'
+alias all='npx ts-node scripts/extract-all.ts -e'
+alias single='npx ts-node scripts/extract-all.ts -e -d'
+alias test='npm run test'
+alias rmst='rm -rf .../.cache/standardized-text/'
+alias rmocr='rm -rf .../.cache/ocr/'
